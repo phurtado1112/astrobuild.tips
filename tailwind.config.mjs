4290af89fda2de0c3f3d0219956a767e3ac673f0
Pablo Hurtado <administracione@phdsystems.net>
2024-09-27 20:03:06 -0600

    Primer commit del sitio

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+    theme: {
+        extend: {},
+        animation: {
+            border: 'background ease infinite',
+          },
+          keyframes: {
+            background: {
+              '0%, 100%': { backgroundPosition: '0% 50%' },
+              '50%': { backgroundPosition: '100% 50%' },
+            },
+          },
+    },
+    plugins: [],   
+}
